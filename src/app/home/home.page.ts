119a2bd733a3fe0c19cb49de7b855d056b6cc502
Murtaza Aun Ali <murtaza.dev@gmail.com>
2019-09-20 10:08:47 +0500

    Products, Categories and Videos services created. Homepage filled with live data.

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { ProductsService } from './../services/products.service';
+import { CategoriesService } from './../services/categories.service';
 import { ProductModel } from '../models/product-model';
+import { VideosService } from './../services/videos.service';
 
 @Component({
     selector: 'app-home',
@@ -19,12 +21,25 @@ export class HomePage {
     };
     products: ProductModel[];
 
-    constructor(private productsService: ProductsService) {
+    categories: any;
+
+    videos: any;
+
+    constructor(private productsService: ProductsService, private categoriesService: CategoriesService, private videosService: VideosService) {
         this.productsService.getProducts().subscribe(products => {
             this.products = products.records;
             console.log(this.products);
         });
 
+        this.categoriesService.getCategories().subscribe(categories => {
+            this.categories = categories.records;
+            console.log(this.categories);
+        });
+
+        this.videosService.getVideos().subscribe(videos => {
+            this.videos = videos.records;
+            console.log(this.videos);
+        });
     }
 
 }
---
+++ src/app/services/videos.service.ts
@@ -0,0 +1,40 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+import { ProductModel } from './../models/product-model';
+import { Observable, throwError, of } from "rxjs";
+import { map, catchError, tap } from "rxjs/operators";
+
+@Injectable({
+    providedIn: 'root'
+})
+export class VideosService {
+    baseUrl: string = "https://bikeboerse.com/BikeApi/ios";
+    constructor(private httpClient: HttpClient) { }
+
+    public getVideos() {
+        return this.httpClient.get(this.baseUrl + '/product/videos.php')
+            .pipe(
+                tap(_ => this.log('response received')),
+                catchError(this.handleError('getVideos', []))
+            );
+    }
+
+    private handleError<T>(operation = 'operation', result?: T) {
+        return (error: any): Observable<T> => {
+
+            // TODO: send the error to remote logging infrastructure
+            console.error(error); // log to console instead
+
+            // TODO: better job of transforming error for user consumption
+            this.log(`${operation} failed: ${error.message}`);
+
+            // Let the app keep running by returning an empty result.
+            return of(result as T);
+        };
+    }
+
+    /** Log a HeroService message with the MessageService */
+    private log(message: string) {
+        console.log(message);
+    }
+}
