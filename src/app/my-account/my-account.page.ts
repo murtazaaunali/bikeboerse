0cc324fd5633b8a2f9bf11663b7d67a481507469
Murtaza Aun Ali <murtaza.dev@gmail.com>
2019-10-02 02:31:11 +0500

    Account controller bug

--- src/app/my-account/my-account.page.ts
+++ src/app/my-account/my-account.page.ts
@@ -1,12 +1,12 @@
 import { Router } from '@angular/router';
 import { Component, OnInit, ChangeDetectorRef } from '@angular/core';
-import { Camera, CameraOptions, PictureSourceType } from '@ionic-native/Camera/ngx';
+/* import { Camera, CameraOptions, PictureSourceType } from '@ionic-native/Camera/ngx';
 import { ActionSheetController, ToastController, Platform, LoadingController } from '@ionic/angular';
 import { File, FileEntry } from '@ionic-native/File/ngx';
 import { HttpClient } from '@angular/common/http';
 import { Storage } from '@ionic/storage';
 import { FilePath } from '@ionic-native/file-path/ngx';
-import { AuthenticationService } from './../services/authentication.service';
+import { AuthenticationService } from './../services/authentication.service'; */
 
 @Component({
     selector: 'app-my-account',
@@ -16,23 +16,23 @@ import { AuthenticationService } from './../services/authentication.service';
 export class MyAccountPage implements OnInit {
     images = [];
 
-    constructor(private authenticationService: AuthenticationService, private router: Router, private camera: Camera, private file: File, private http: HttpClient,
+    constructor(/* private authenticationService: AuthenticationService, private router: Router, private camera: Camera, private file: File, private http: HttpClient,
         private actionSheetController: ActionSheetController, private toastController: ToastController,
         private storage: Storage, private platform: Platform, private loadingController: LoadingController,
-        private ref: ChangeDetectorRef, private filePath: FilePath) {
-        this.authenticationService.authenticationState.subscribe(state => {
+        private ref: ChangeDetectorRef, private filePath: FilePath */) {
+        /* this.authenticationService.authenticationState.subscribe(state => {
             if (!state) {
                 this.router.navigate(['login']);
             } else {
                 this.router.navigate(['my-account']);
             }
-        });
+        }); */
     }
 
     ngOnInit() {
     }
 
-    async selectImage() {
+    /* async selectImage() {
         const actionSheet = await this.actionSheetController.create({
             header: "Select Image source",
             buttons: [{
@@ -80,5 +80,5 @@ export class MyAccountPage implements OnInit {
             n = d.getTime(),
             newFileName = n + ".jpg";
         return newFileName;
-    }
+    } */
 }
